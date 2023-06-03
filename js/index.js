40c9ad63e1c40e52d53d42c124eeea0229dcd68d
Alexandra Hernandez Duarte <alexa.her.duarte@gmail.com>
2023-06-03 17:25:37 -0500

    Cambio en variables

--- js/index.js
+++ js/index.js
@@ -5,15 +5,15 @@ const selectElement = function(element) {
 
 
 let menuToggle = selectElement('.menu-toggle');     //Selecciona el menu
-let equixxxxx = selectElement('.fa-times');
-let barssss = selectElement('.fa-bars');
+let x = selectElement('.fa-times');
+let bars = selectElement('.fa-bars');
 let navList = selectElement('.nav-list');
 
 
 menuToggle.addEventListener('click', function(){        //Abre el menu
     navList.classList.toggle('open');
-    equixxxxx.classList.toggle('open');
-    barssss.classList.toggle('open')
+    x.classList.toggle('open');
+    bars.classList.toggle('open')
 })
 
 let nosotros = selectElement('.nosotros-btn');
@@ -27,7 +27,7 @@ nosotros.addEventListener('click', function(){
 let menuToggle2 = selectElement('.menu-toggle2');     //Selecciona el menu
 let navList2 = selectElement('.nav-list2')
 menuToggle2.addEventListener('click', function(){  
-    equixxxxx.classList.toggle('open');
-    barssss.classList.toggle('open')      //Abre el menu
+    x.classList.toggle('open');
+    bars.classList.toggle('open')      //Abre el menu
     navList2.classList.toggle('open');
 })
